2774771a88ca9332fd39f33c5282ace059247d39
Dev-Etto <contato.devetto@gmail.com>
2026-03-16 22:02:26 -0300

    fix: remove sponsors

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -2,6 +2,5 @@ export const EXTERNAL_LINKS = {
   BOT_INVITE: import.meta.env.VITE_BOT_INVITE_URL || 'https://discord.com',
   GITHUB_OWNER: import.meta.env.VITE_GITHUB_OWNER_URL || 'https://github.com/Dev-Etto',
   GITHUB_REPO: import.meta.env.VITE_GITHUB_REPO_URL || 'https://github.com/Dev-Etto/voice-league',
-  GITHUB_SPONSORS: import.meta.env.VITE_GITHUB_SPONSORS_URL || '#',
   BUY_ME_A_COFFEE: import.meta.env.VITE_BUY_ME_A_COFFEE_URL || '#',
 } as const;
